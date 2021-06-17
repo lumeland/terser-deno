82148c85476ed19ebd3b859823f5059fed089265
Oscar Otero <oom@oscarotero.com>
2021-06-17 14:01:22 +0200

    first commit

---
+++ deno/lib/deps.ts
@@ -0,0 +1,7 @@
+// @deno-types="https://deno.land/x/source_map@0.7.4/source-map.d.ts"
+import * as SourceMap from "https://deno.land/x/source_map@0.7.4/mod.js";
+
+export const MOZ_SourceMap = SourceMap;
+export type RawSourceMap = SourceMap.RawSourceMap;
+
+export { Buffer } from "https://deno.land/std@0.98.0/node/buffer.ts";
---
+++ to_deno.js
@@ -0,0 +1,30 @@
+import { convert } from "https://deno.land/x/nodedeno@v0.2.9/mod.js";
+
+// Convert the code
+await convert({
+  src: "terser",
+  input: ["lib", "tools", "main.js"],
+  output: "deno",
+  transpile: false,
+  modules: {
+    "deps.js": "lib/deps.ts",
+  },
+  copy: {
+    "deps.ts": "lib/deps.ts",
+    "terser/README.md": "README.md",
+    "terser/CHANGELOG.md": "CHANGELOG.md",
+    "terser/LICENSE": "LICENSE",
+  },
+  beforeConvert(src, { rename }) {
+    src.delete("lib/cli.js");
+    rename(
+      "main.js",
+      "mod.js",
+      (code) =>
+        code.replace(
+          'export { run_cli as _run_cli } from "./lib/cli.js";\n',
+          "",
+        ),
+    );
+  },
+});
