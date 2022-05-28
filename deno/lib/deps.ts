c32fb04cd2d4b42ec6cb94a2a6e4ae44156fbdfc
Oscar Otero <oom@oscarotero.com>
2022-05-28 15:06:00 +0200

    updated deps

--- deno/lib/deps.ts
+++ deno/lib/deps.ts
@@ -1,7 +1,7 @@
-// @deno-types="https://deno.land/x/source_map@0.7.4/source-map.d.ts"
-import * as SourceMap from "https://deno.land/x/source_map@0.7.4/mod.js";
+// @deno-types="https://deno.land/x/source_map@0.7.5/source-map.d.ts"
+import * as SourceMap from "https://deno.land/x/source_map@0.7.5/mod.js";
 
 export const MOZ_SourceMap = SourceMap;
 export type RawSourceMap = SourceMap.RawSourceMap;
 
-export { Buffer } from "https://deno.land/std@0.100.0/node/buffer.ts";
+export { Buffer } from "https://deno.land/std@0.140.0/node/buffer.ts";
